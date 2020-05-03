140d0104d8a3a181d9533126641e6ccb5c8e979a
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2020-05-03 16:57:27 -0500

    Add files via upload

---
+++ Sem_foro_EDA.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  delay(2000);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  delay(2000);
+}
