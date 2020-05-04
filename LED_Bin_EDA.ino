c47b945f6726440a7ef394de7a728d62b7d46a3c
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2020-05-03 19:55:06 -0500

    Add files via upload

---
+++ LED_Bin_EDA.ino
@@ -0,0 +1,39 @@
+int estado=0;
+int estadoAnt=0;
+int salida=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  estado=digitalRead(8);
+  if ((estado==HIGH) && (estadoAnt==LOW))
+  {
+    salida=3-salida;
+    delay(20);
+  }
+
+  estadoAnt = estado;
+  switch(salida){
+    case 3:
+        digitalWrite(10,HIGH);
+        digitalWrite(9,LOW);
+    break;
+    case 2:
+        digitalWrite(10,LOW);
+        digitalWrite(9,HIGH);
+    break;
+    case 1:
+        digitalWrite(10,HIGH);
+        digitalWrite(9,HIGH);
+    break;
+    default:
+        digitalWrite(10,LOW);
+        digitalWrite(9,LOW);
+    break;
+  }
+}
